80b0f97486b13d43b4c5f1d069209df7fb1b8d5b
hejin <286891132@qq.com>
2017-04-21 22:26:22 +0800

    'diyitian'

--- components/home/home.js
+++ components/home/home.js
@@ -10,19 +10,50 @@ angular.module('homeModule',[])
 })
 
 //依赖注入
-.service('swiper',['$timeout',function($timeout){
-	$timeout(function(){
-		new Swiper ('.swiper-container', {
-		    loop: true,
-		    autoplay:500,
-		    // 如果需要分页器
-		    pagination: '.swiper-pagination',
-		  }) 
-	},10);
-}])
+//.service('swiper',['$timeout',function($timeout){
+//	$timeout(function(){
+//		new Swiper ('.swiper-container', {
+//		    loop: true,
+//		    autoplay:500,
+//		    // 如果需要分页器
+//		    pagination: '.swiper-pagination',
+//		  }) 
+//	},10);
+//}])
 
-.controller('homeCtrl',['$scope','$http','swiper',function($scope,$http){
-//	$http.get('json/09.json').success(function(res){
-//		console.log(res)
-//	})
+.controller('homeCtrl',['$scope','$http',function($scope,$http){
+	$scope.isChang1=true
+	$scope.isChang2=false
+	$scope.isChang2=false
+	$http.get('json/topContent.json').success(function(res){
+		console.log(res.data);
+		$scope.home_head=res.data[32377];
+		$scope.home_up=res.data[5956];
+		$scope.home_menu=res.data[13730];
+	})
+	$http.get('json/niuzaiku.json').success(function(res){
+		console.log(res.data);
+		$scope.home_menu_next=res.data
+	})
+	$http.get('json/moren.json').success(function(res){
+		console.log(res.data);
+		$scope.moren=res.data.list;
+		$scope.home_tab_main=$scope.moren;
+		
+	})
+	$http.get('json/jingxuan.json').success(function(res){
+		console.log(res.data);
+		$scope.jingxuan=res.data.list;
+		
+	})
+	$http.get('json/zuixin.json').success(function(res){
+		console.log(res.data);
+		$scope.zuixin=res.data.list;
+	
+	})
+	$scope.isClick=function(datas){
+		$scope.home_tab_main=datas;	
+	}
+	
+	
 }])
