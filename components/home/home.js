80cc856acb39e7d4a42972de42abfabf58bdedc7
hejin <286891132@qq.com>
2017-04-21 11:17:48 +0800

    '123'

--- components/home/home.js
+++ components/home/home.js
@@ -8,8 +8,21 @@ angular.module('homeModule',[])
             css:'components/home/home.css'
         })
 })
-.controller('homeCtrl',['$scope','$http',function($scope,$http){
-	$http.get('json/09.json').success(function(res){
-		console.log(res)
-	})
+
+//依赖注入
+.service('swiper',['$timeout',function($timeout){
+	$timeout(function(){
+		new Swiper ('.swiper-container', {
+		    loop: true,
+		    autoplay:500,
+		    // 如果需要分页器
+		    pagination: '.swiper-pagination',
+		  }) 
+	},10);
+}])
+
+.controller('homeCtrl',['$scope','$http','swiper',function($scope,$http){
+//	$http.get('json/09.json').success(function(res){
+//		console.log(res)
+//	})
 }])
