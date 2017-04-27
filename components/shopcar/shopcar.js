83628e231d65621aaa0ecd89104f3baf0a744a61
hejin <286891132@qq.com>
2017-04-27 20:47:20 +0800

    'car'

--- components/shopcar/shopcar.js
+++ components/shopcar/shopcar.js
@@ -9,5 +9,28 @@ angular.module('shopcarModule',[])
         })
 })
 .controller('shopcarCtrl',['$scope',function($scope){
-	
+	var local=localStorage.getItem('carGoods1');
+	if(local){
+		$scope.goodsArr=JSON.parse(local);
+		console.log($scope.goodsArr);
+		$("#products").hide();
+		$("#shopcar_main").show();
+		$(".delete").text('删除');
+		var totalP=0;
+		for(var i=0;i<$scope.goodsArr.length;i++){
+			var pic=$scope.goodsArr[i].price.substring(1);
+			totalP+=pic*$scope.goodsArr[i].num
+		}
+		$("#carPic span").text('￥'+totalP)
+	}
+	$scope.toDelete=function(){
+		localStorage.clear();
+		$("#products").show();
+		$("#shopcar_main").hide();
+		$(".delete").text('首页');
+	}
+	$scope.slide=function(){
+		$("#shopcarM_m").slideToggle();
+		$("#shopcarM_h>span").toggleClass('slideInUp');
+	}
 }])
